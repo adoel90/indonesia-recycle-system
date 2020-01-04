ff72a8b18b68516aff96895f10f71bfa7408e05f
Adoel Rachman <adoel.cs@gmail.com>
2020-01-04 21:42:58 +0700

    Updates

---
+++ precache-manifest.fe50469b16294a36dbac2f234322e719.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcd33ef2f1b168b2ef6aab4ed4bb524",
+    "url": "/indonesia-recycle-system/index.html"
+  },
+  {
+    "revision": "7d444e4968a758d41830",
+    "url": "/indonesia-recycle-system/static/css/2.0d743cc7.chunk.css"
+  },
+  {
+    "revision": "7d444e4968a758d41830",
+    "url": "/indonesia-recycle-system/static/js/2.86ec17cb.chunk.js"
+  },
+  {
+    "revision": "c4f7ad23bbefe1630191",
+    "url": "/indonesia-recycle-system/static/js/main.a94768d0.chunk.js"
+  },
+  {
+    "revision": "b6d66b5a00062c5e3ce7",
+    "url": "/indonesia-recycle-system/static/js/runtime-main.34b8b09e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/indonesia-recycle-system/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "7c43bd22524b4b677a9d86f12a9b4a4e",
+    "url": "/indonesia-recycle-system/static/media/map_gojek2.7c43bd22.jpg"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/indonesia-recycle-system/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  },
+  {
+    "revision": "4f3e4f58ad8bd3a5eb33fa220132922f",
+    "url": "/indonesia-recycle-system/static/media/sampah_boncos.4f3e4f58.jpeg"
+  },
+  {
+    "revision": "dc74916de93e254da314ad06dbdaa03b",
+    "url": "/indonesia-recycle-system/static/media/sampah_botol_bening.dc74916d.jpeg"
+  },
+  {
+    "revision": "8010bafa9cf956068822d6b781be2cd1",
+    "url": "/indonesia-recycle-system/static/media/sampah_gabruk.8010bafa.jpeg"
+  }
+]);
